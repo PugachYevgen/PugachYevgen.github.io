257e3ef3f916a86190f119efe769bfcd07f47067
USER-PC\Tom <jedi.wasya@gmail.com>
2020-05-11 14:38:39 +0300

    1105

--- team/js/common.js
+++ team/js/common.js
@@ -2,7 +2,7 @@ $(function () {
 
 
     $(document).ready(function () {
-        $(".owl-carousel").owlCarousel({
+        $(".news-carousel").owlCarousel({
             loop: true,
             items: 3,
             autoplay: false,
@@ -29,6 +29,66 @@ $(function () {
                 },
             }
 
+        });
+
+    });
+
+    $(document).ready(function () {
+        $(".quote-carousel").owlCarousel({
+            loop: true,
+            items: 1,
+            autoplay: false,
+            autoHeight: true,
+            nav: true,
+            navText: "",
+            responsive: {
+                0: {
+                    items: 1,
+                    margin: 20,
+                    nav: false,
+                    autoplay: false,
+                },
+                575: {
+                    nav: true
+                },
+                960: {
+                    items: 1,
+                    nav: true,
+                },
+                1000: {
+                    items: 1,
+                },
+            }
+
+        });
+    });
+
+    $(document).ready(function() {
+
+        $('.wrapper>p').not(':first-of-type').hide();
+
+
+        $('.wrapper>span').click(function() {
+
+            var findArticle = $(this).next();
+            var findWrapper = $(this).closest('.wrapper');
+
+            if (findArticle.is(':visible')) {
+                $(this).removeClass('span-active');
+                findArticle.slideUp('fast');
+                findArticle.removeClass('active');
+
+
+            }
+            else {
+
+                findWrapper.find('>p').slideUp('fast').removeClass('span-active');
+                findWrapper.find('>.span-active').not(this).removeClass('span-active');
+                findArticle.slideDown('fast');
+                findArticle.addClass('active');
+                $(this).addClass('span-active');
+
+            }
         });
 
     });
